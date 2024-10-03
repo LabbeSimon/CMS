b6da768cbd95c1292c0e2fc36649f00d460eee23
Simon Labbé <contact.asgarp@gmail.com>
2024-10-03 18:05:55 +0200

    Add files via upload

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,5 @@
+<?php
+?>
+<h1>Bienvenue sur le CMS</h1>
+<p>Je peut modifié cette page en créant un compte sur <a href=/register.php>s'enregistré</a></p>
 
---
+++ pages/page1.php
@@ -0,0 +1,2 @@
+<h1>Page 1</h1>
+<p>Bienvenue sur la page 1 de notre CMS.</p>
